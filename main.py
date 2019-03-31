78d18b06bc353fed5c6da009b92526182643dd26
Jakub Mularski <jakubmularski723@gmail.com>
2019-03-31 22:03:26 +0200

    Server rewrital and fixes

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 from flask import Flask, jsonify, request
 from pymongo import MongoClient
 import re
+from json import dumps
 #app = Flask(__name__)
 
 client = MongoClient()
 db = client.bundestag_analysis
-posts = db.posts
+data = db.data
 
 def getAttendanceAtDate(date, party=None):
     unique_speakers = []
     query = {"date": date}
     if party is not None:
         query['fraction_or_title'] = party
-    speakers = posts.find(query, {"_id": 0, "name": 1})
+    speakers = data.find(query, {"_id": 0, "name": 1})
     for speaker in speakers:
         if speaker['name'] not in unique_speakers:
             unique_speakers.append(speaker['name'])
     result = { 'fulfillmentText': "That many: " + str(len(unique_speakers)) }
-    return jsonify(result)
+    return result
 
 
 def getSpeakingTime(person, date=""):
@@ -25,53 +26,28 @@ def getSpeakingTime(person, date=""):
     query = {"name": person}
     if date is not None:
         query['date'] = date
-    texts = posts.find(query, {"_id": 0, "text": 1})
+    texts = data.find(query, {"_id": 0, "text": 1})
     for text in texts:
         time += len(text['text']) / 130
-    return time
+    result = { 'fulfillmentText': 'Person ' + parameters[u'person'] + ' was speaking at this meeting for ' + str(time) + ' minutes.' }
+    return result
 
 
 def getMostDiscussedTopics(party):
-    texts = posts.find({"party": party}, {"_id": 0, "text": 1})
+    texts = data.find({"party": party}, {"_id": 0, "text": 1})
+    result = { 'fulfillmentText': json.dumps(texts) }
+    return result
 
-# @app.route('/', methods = ['POST'])
-
-
+@app.route('/', methods = ['POST'])
 def dialogflow():
-    body = request.json[u'queryResult']
+    body = request[u'json']
     intent = body[u'intent'][u'displayName']
     parameters = body[u'parameters']
-    if intent == 'date/party':
-        return getAttendanceAtDate(parameters[u'date'], parameters[u'party'])
-        result = {
-            'fulfillmentText': "That many: " + str(result)
-        }
-        return jsonify(result)
-    elif intent == 'date/total':
-        result = getAttendanceAtDate(parameters[u'date'])
-        result = {
-            'fulfillmentText': 'In total: ' + str(result[1])
-        }
-        return jsonify(result)
-    elif intent == 'data/speaking_time':
-        result = getSpeakingTime(parameters[u'date'], parameters[u'person'])
-        result = {
-            'fulfillmentText': 'Person ' + parameters[u'person'] + ' was speaking at this meeting for ' + str(result) + ' minutes.'
-        }
-        return jsonify(result)
-    elif intent == 'date/speaking_time_total':
-        result = getSpeakingTime(parameters[u'person'])
-        result = {
-            'fulfillmentText': json.dumps(result)
-        }
-        return jsonify(result)
-    elif intent == 'party/topics':
-        result = getMostDiscussedTopics(parameters[u'party'])
-        result = {
-            'fulfillmentText': json.dumps(result)
-        }
-        return jsonify(result)
-
+    if intent == 'date/party': return jsonify(getAttendanceAtDate(parameters[u'date'], parameters[u'party']))
+    elif intent == 'date/total': return jsonify(getAttendanceAtDate(parameters[u'date']))
+    elif intent == 'data/speaking_time': return jsonify(getSpeakingTime(parameters[u'date'], parameters[u'person']))
+    elif intent == 'date/speaking_time_total': return jsonify(getSpeakingTime(parameters[u'person']))
+    elif intent == 'party/topics': return jsonify(getMostDiscussedTopics(parameters[u'party']))
 
 if __name__ == "__main__":
     app.run()
